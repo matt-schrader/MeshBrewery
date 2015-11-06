e70faeea3b07674c5e8205a990ad6f6ab3905ff4
Matthew Schrader <SchraderMJ11@gmail.com>
2015-11-05 21:15:15 -0600

    Add restivus package and enable it

--- brew-controller.js
+++ brew-controller.js
@@ -1,6 +1,14 @@
 Tasks = new Mongo.Collection("tasks");
+Devices = new Mongo.Collection("devices");
 
 if (Meteor.isServer) {
+  var Api = new Restivus({
+    useDefaultAuth: true,
+    prettyJson: true
+  });
+
+  Api.addCollection(Tasks);
+
   Meteor.startup(function () {
     // code to run on server at startup
   });
